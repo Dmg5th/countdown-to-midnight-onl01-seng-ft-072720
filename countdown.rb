fa7b04d41e707978cef9e0366e5ea53119ddd0c6
Daniel Gould <dmgould310@gmail.com>
2020-07-29 11:54:31 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -4,9 +4,15 @@ def countdown(integer)
   while integer > 0
   puts "#{integer} SECOND(S)!"
   integer -= 1 
-end
+  end
   return "HAPPY NEW YEAR!"
 end
 
 def countdown_with_sleep(int)
-  
+  sleep 1 
+  while integer > 0
+  puts "#{integer} SECOND(S)!"
+  integer -= 1 
+  end
+  return "HAPPY NEW YEAR!
+end 
